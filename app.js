c1edac7e5b902d5d0202662823f2a92275c2b25e
Raymond Leon <raymondleon@raymondleon.ca>
2018-04-24 12:08:34 -0400

    Basic data entry

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
+var bodyParser =  require('body-parser');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var userInputOutputRouter = require('./routes/input_output');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var userInputOutputRouter = require('./routes/input_output')
 
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/input_output', userInputOutputRouter);
 app.use('/users', usersRouter);
--- routes/input_output.js
+++ routes/input_output.js
@@ -1,17 +1,40 @@
-var express = require('express')
-var router = express.Router()
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+var Server = require('mongodb').Server
+
+var express = require('express');
+var router = express.Router();
+var database_url = 'mongodb://localhost:27017'
+var database_name = 'test'
+
+var mongodb_url = 'mongodb://localhost:27017'
 
 router.get('/', function(req, res, next) {
   res.render('input_output', {})
 })
 
 router.get('/test', function(req, res, next) {
+  MongoClient.connect(mongodb_url, function(err, client){
+    assert.equal(null,err)
+    console.log('successfully connected')
+    client.close()
+  })
   res.send('test 2');
-//  res.render('input_output', {})
 })
 
 router.post('/', function(req,res,next){
-  res.render('input_output', {test: 'data output'})
+  MongoClient.connect(mongodb_url, function(err, client) {
+    if(err) {
+      return console.dir(err)
+    }
+    var db = client.db(database_name)
+    var collection = db.collection('ratings')
+  
+    collection.insert({'rating': req.body.rating, 'name': req.body.name, 'desription': req.body.description})
+  
+    client.close()
+  });
+  res.render('input_output', {})
 })
 
 module.exports = router
